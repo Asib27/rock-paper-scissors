fc4a81efa51ae6dcc720e63d54544ab812747fa7
asib27 <asib.rahman1927@gmail.com>
2022-05-07 21:27:34 +0800

    Add playerPlay() and ComputerPlay()

--- scripts.js
+++ scripts.js
@@ -1,1 +1,12 @@
-console.log("Test");
+playerPlay = ()=>{
+    return prompt("Enter rock, paper or scissors").toLowerCase();
+}
+
+computerPlay = ()=>{
+    let chooseFrom = ['rock', 'paper', 'scissors'];
+    let choice = Math.floor(Math.random() * 3);
+    return chooseFrom[choice];
+}
+
+console.log(playerPlay())
+console.log(computerPlay())
