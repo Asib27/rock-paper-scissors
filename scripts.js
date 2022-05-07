85d1136a50fb0833ab18996ffb6e027a86b6b563
asib27 <asib.rahman1927@gmail.com>
2022-05-07 21:47:23 +0800

    Add console based game

--- scripts.js
+++ scripts.js
@@ -2,11 +2,55 @@ playerPlay = ()=>{
     return prompt("Enter rock, paper or scissors").toLowerCase();
 }
 
+let chooseFrom = ['rock', 'paper', 'scissors'];
 computerPlay = ()=>{
-    let chooseFrom = ['rock', 'paper', 'scissors'];
     let choice = Math.floor(Math.random() * 3);
     return chooseFrom[choice];
 }
 
-console.log(playerPlay())
-console.log(computerPlay())
+playRound = (playerSelection, computerSelection)=>{
+    playerIdx = chooseFrom.indexOf(playerSelection);
+    computerIdx = chooseFrom.indexOf(computerSelection);
+
+    if((playerIdx + 1) % chooseFrom.length == computerIdx){
+        return 1; // computer wins
+    }
+    else if(playerIdx == computerIdx) return 0; // tie
+    else{
+        return -1;
+    }
+}
+
+play = (no_of_games)=>{
+    let computerWin = 0;
+    let playerWin = 0;
+
+    for(let i =0; i < no_of_games; i++){
+        let computerChoice = computerPlay();
+        let playerChoice = playerPlay();
+
+        let result = playRound(playerChoice, computerChoice);
+        if(result == 1){
+            computerWin++;
+            console.log('Computer Wins, ', computerChoice, 'beats', playerChoice);
+        }
+        else if(result == -1){
+            playerWin++;
+            console.log('Player Wins, ', playerChoice, 'beats', computerChoice);
+        }
+        else{
+            console.log(`Its a tie: ${computerChoice} ${playerChoice} ties`);
+            i--;
+        }
+
+        console.log("Current Score : player ", playerWin, " - ", computerWin, "computer")
+    }
+
+    if(computerWin > playerWin){
+        console.log(`Computer wins by ${computerWin} - ${playerWin}`)
+    }else{
+        console.log(`Player wins by ${playerWin} - ${computerWin}`)
+    }
+}
+
+play(5);
